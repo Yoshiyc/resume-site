027415159902852c4831bc303847ece5bcd61d78
Yoshiyc <yongyuc7@gmail.com>
2025-07-11 10:19:45 +0800

    改用 @sveltejs/adapter-vercel 支援 Vercel 部署

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-vercel';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess({
+	preprocess: sveltePreprocess({
 		typescript: true,
 		postcss: true
 	}),
